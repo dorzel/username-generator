59d35b0483a7b4cdc937cf288a46504a31e903a9
Dylan Orzel <dylan.orzel@gmail.com>
2017-08-31 12:14:43 -0600

    add more custom text for pokemon, add difflib matching

--- generate_username.py
+++ generate_username.py
@@ -7,7 +7,7 @@ from sys import argv
 corpora_dict = {}
 
 
-def load_tag_pickles():
+def load_corpus_pickles():
     for filename in os.listdir("pre-generated-lists/"):
         if filename.endswith(".pkl"):
             corpus_name = filename.split("_")[-1].rstrip(".pkl")
@@ -18,14 +18,26 @@ def load_tag_pickles():
 
 
 def select_data(segment):
-    """ Select relevant tags based on what was specified from the cli. """
+    """ Select relevant tags based on what was specified from the cli. Returns
+    a segment of a username as a string.
+    """
     char_mapping = {"V": "VERB", "N": "NOUN", "A": "ADJ"}
 
     if len(segment) > 1:
         # trying to specify a certain corpus like "Pokemon-N"
         corpus_name = segment.split('-')[0]
         tag_type = segment.split('-')[-1]
-        return choice(corpora_dict[corpus_name][char_mapping[tag_type]])
+        try:
+            tag = choice(corpora_dict[corpus_name][char_mapping[tag_type]])
+        except KeyError:
+            from difflib import get_close_matches
+            print("Could not find custom corpus '{}'. Did you mean: {}?"
+                  .format(corpus_name,
+                          get_close_matches(corpus_name,
+                                            list(corpora_dict.keys()))))
+            tag = ""
+        finally:
+            return tag
     else:
         # just a regular N,V,A, etc. Collect all data here.
         tags = []
@@ -35,8 +47,8 @@ def select_data(segment):
 
 
 def generate_username(name_format):
-    """ Generates a username with the given format, choosing from any
-    combination of V,N,A (Verb, Noun, Adjective)
+    """ Generates a username as string with the given format, choosing from any
+    combination of V,N,A (Verb, Noun, Adjective) from any specified corpora. 
     """
     username = ""
     for segment in name_format:
@@ -50,5 +62,5 @@ if __name__ == "__main__":
         name_format = argv[1:]
     else:
         name_format = ["A", "N"]
-    load_tag_pickles()
+    load_corpus_pickles()
     print(generate_username(name_format=name_format))
