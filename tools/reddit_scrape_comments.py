7fecf87409caa1b3a5ac771b2a4d50aad5612603
Dylan Orzel <dylan.orzel@gmail.com>
2017-09-15 15:26:43 -0600

    working reddit scraper

--- tools/reddit_scrape_comments.py
+++ tools/reddit_scrape_comments.py
@@ -4,7 +4,7 @@ import platform
 import praw
 from uuid import uuid4
 
-MAX_FILE_SIZE = 10000
+MAX_FILE_SIZE = 10000000  # 10 MB
 username = sys.argv[1]
 password = sys.argv[2]
 client_id = sys.argv[3]
@@ -19,23 +19,34 @@ reddit = praw.Reddit(client_id=client_id,
 
 def get_file_object():
     file_obj = None
-    for file in os.listdir("../custom-corpora/reddit/"):
-        if os.stat(file).st_size < MAX_FILE_SIZE and file.endswith(".txt"):
+    base_dir = "../custom-corpora/reddit/{}"
+    for file in os.listdir(base_dir.format("")):
+        if os.stat(base_dir.format(file)).st_size < MAX_FILE_SIZE and \
+                file.endswith(".txt"):
             # find the first file that can be appended to
-            file_obj = open("../custom-corpora/reddit/{}.txt".format(uuid4()),
-                            "a")
+            file_obj = open(base_dir.format(file), "a")
             break
     else:
         # return a new file for writing if all files in the directory are
         # already at max size
-        file_obj = open("../custom-corpora/reddit/{}.txt".format(uuid4()), "w")
+        file_obj = open(base_dir.format(str(uuid4()) + '.txt'), "w")
     return file_obj
 
 
-def write_comment(comment):
+def write_comment(comment_body):
+    # split comment into nltk-compatible sentences and write out to file.
+    # TODO: don't open the file for every single write...
     with get_file_object() as outfile:
-        outfile.write(comment + "\n")
+        sentences = [sentence.strip() + "." for sentence in
+                     comment_body.split(".") if
+                     sentence.replace(" ", "").strip().isalnum() and
+                     len(sentence.split()) > 1]
+        for sentence in sentences:
+            outfile.write(sentence + "\n")
 
 if __name__ == "__main__":
-    for comment in praw.models.reddit.subreddit.SubredditStream.comments():
-        print(comment)
+    try:
+        for comment in reddit.subreddit("all").stream.comments():
+            write_comment(comment.body)
+    except KeyboardInterrupt:
+        print("Done.")
